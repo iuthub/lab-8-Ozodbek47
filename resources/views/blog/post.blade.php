3b2930044b1c96c39baa8168adead2e5babbcd78
Ozodbek47 <ozodbek.kuchkarov@gmail.com>
2019-04-07 13:33:12 +0500

    2.1 Blog Section Done
    Student: Ozodbek Kuchkarov
    ID: u1710224

---
+++ resources/views/blog/post.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">Learning Laravel</p>
+        </div>
+    </div>
+
+    <p>Lorem ipsum something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something something</p>
+
+@endsection
