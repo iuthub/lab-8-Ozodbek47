7533c762c245d539701c4845683f381a49e0e630
Ozodbek47 <ozodbek.kuchkarov@gmail.com>
2019-04-20 23:08:59 +0500

    Lab 8 Finished
    Student: Ozodbek Kuchkarov
    ID: u1710224

---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class=" quote post-title">About me</h1>
+            <p>Lorem ipsum ashifghigfh gsdfgv fhsd fsdhfdsg fh fdsgfh ghsdgfg hfsdghfgdsfsdj
+            fsdhfdshf dsfghdsgf hfsdgfhgsdugf usduhfgu hsgdfuhgs hgfdhusg usdg hufsdghufgsdhufudsgfu 
+        	fsdhjfisdhfh hsdijfhdsihfijdsh fijsdh fijsh idjh idhsiufhisudhf fdsuihfiusd </p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
+
+@endsection
